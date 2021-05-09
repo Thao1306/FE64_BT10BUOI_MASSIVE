49ac971bb2082e8eaeee9be4371f440246ab3e90
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-05-09 22:50:05 +0700

    [Massive]: Add Massive head parts

---
+++ JS/main.js
@@ -0,0 +1,50 @@
+/*==============================================
+  Add Sticky Header
+===============================================*/
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 100) {
+    $(".header").addClass("header-sticky");
+  } else {
+    $(".header").removeClass("header-sticky");
+  }
+});
+
+/*==============================================
+  Init Wow
+===============================================*/
+new WOW().init();
+
+/*==============================================
+  Init Portfolio Popup
+===============================================*/
+$(".js-portfolio-popup").magnificPopup({
+  type: "image",
+  gallery: {
+    enabled: true,
+  },
+});
+
+$(".js-portfolio-popup2").magnificPopup({
+  type: "image",
+  gallery: {
+    enabled: true,
+  },
+});
+
+/*==============================================
+  Add Sticky Header
+===============================================*/
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 100) {
+    $(".header").addClass("header-sticky");
+  } else {
+    $(".header").removeClass("header-sticky");
+  }
+});
+
+// Test
+// var containerEl = $(".js-portfolio-list");
+// var mixer = mixitup(containerEl);
+
+var containerEl = $(".portfolio-list");
+var mixer = mixitup(containerEl);
