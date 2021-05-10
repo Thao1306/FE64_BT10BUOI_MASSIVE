ee1702e76eda3bb67d029c2994db373fcf124852
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-05-10 22:17:03 +0700

    [Massive]: Handle Progress bar

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,22 @@
 /*==============================================
-  Add Sticky Header
+  Init Window Scroll Event
 ===============================================*/
 $(window).scroll(function () {
+  handleStickyHeader();
+  handleProgressBar();
+  scrollFunction();
+});
+
+/*==============================================
+  Add Sticky Header
+===============================================*/
+var handleStickyHeader = function () {
   if ($(window).scrollTop() >= 100) {
     $(".header").addClass("header-sticky");
   } else {
     $(".header").removeClass("header-sticky");
   }
-});
+};
 
 /*==============================================
   Init Wow
@@ -32,20 +41,34 @@ $(".js-portfolio-popup2").magnificPopup({
 });
 
 /*==============================================
-  Portfolio Filters
+  Init Portfolio Filter
 ===============================================*/
 var portfolioList = $(".js-portfolio-list");
 var mixer = mixitup(portfolioList);
 
+/*==============================================
+  Handle Progress Bar
+===============================================*/
+var handleProgressBar = function () {
+  var progressBars = document.getElementsByClassName("js-progress-bar");
 
-// back to top 
+  Array.from(progressBars).forEach((bar) => {
+    var windowTop = window.scrollY;
+    var windowBottom = windowTop + window.innerHeight;
+
+    var eleTop = bar.offsetTop - 20;
+    var eleBottom = eleTop + bar.clientHeight;
+
+    if (eleBottom <= windowBottom && eleTop >= windowTop) {
+      bar.classList.add("active");
+    }
+  });
+};
 
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
